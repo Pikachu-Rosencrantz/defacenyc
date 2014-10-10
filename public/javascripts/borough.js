16b6c8cf68696b80fb51b45e1da86b536acfdcd4
Nastassia <nastassiacarmona@gmail.com>
2014-10-10 16:20:05 -0400

    Style the borough page

--- public/javascripts/borough.js
+++ public/javascripts/borough.js
@@ -6,13 +6,13 @@ submit_borough.on('click', function(){
   $('#locations').html(" ");
   // debugger;
   $.get('http://localhost:9292/' + borough, function(data){
-    $('body').append('<table id="locations"></table>');
+    $('body').append('<table class="table table-striped" id="locations"></table>');
     $('#locations').append('<tr><th>Address</th><th>Artist</th></tr>');
     // debugger;
     data = JSON.parse(data);
 
     for(var i=0;i<data.length;i++){
-$('#locations').append('<tr><td>'+data[i]['address']+data[i]['artist_id']+'</td></tr>'); 
+$('#locations').append('<tr><td>'+data[i]['address']+'</td>'+'<td>'+data[i]['artist_id']+'</td></tr>');
     }
   })
 })
