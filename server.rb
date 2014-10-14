2afc347711114af5dcd2093ce6ded14ab34d3bd9
conorhastings <hastings.conorm@gmail.com>
2014-10-14 14:41:20 -0400

    Update server.rb

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
 Bundler.require(:default)
-require_relative './config/environments'
+require_relative './db/connection'
 require_relative './lib/models'
 require 'active_support'
 # require 'pry'
