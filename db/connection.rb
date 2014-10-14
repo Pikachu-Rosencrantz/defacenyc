c7294afce67e482f990e3591472e32b4ab7f7265
conorhastings <hastings.conorm@gmail.com>
2014-10-14 14:40:49 -0400

    Update connection.rb

--- db/connection.rb
+++ db/connection.rb
@@ -2,8 +2,8 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection({
   :adapter => "postgresql",
-  :host => "localhost",
-  :username => "jeffcampomanes",
+  :host => "",
+  :username => "root",
   :database => "deface_nyc"
 })
 
